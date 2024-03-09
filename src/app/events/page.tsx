96c2c3278dd3b0bdfc5920710e5c32ef91ddb5fb
Elias Xu <flam0799@gmail.com>
2024-03-09 12:19:46 -0500

    first commit

---
+++ src/app/events/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default async function Events() {
+    return (<div></div>);
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,97 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from "./Nav.module.css"
+
+import LowerBannerTexture from "/public/static/images/LowerBannerTexture.png";
+import JuniorCaucusLogoSvg from "/public/static/images/JuniorCaucusLogo.svg";
+
+interface NavProps {
+    page: string
+}
+
+export default async function Nav(props: NavProps) {
+    return (
+        <div className={styles.navbar}>
+            <div>
+                <div className={styles.banner}>
+                    <p className={styles["banner-text"]}>
+                        We’re raising money for Junior Prom 2024! Read our initiative{" "}
+                        <Link href="/JProm" className={styles["banner-links"]}>
+                            here
+                        </Link>
+
+                    </p>
+                </div>
+                <div className={styles["banner-texture"]}>
+                    <Image src={LowerBannerTexture} alt="Texture" />
+                </div>
+            </div>
+            <div className={styles["nav-container"]}>
+                <div className={styles["juni-log-container"]}>
+                    <Link href="/" className={`${styles["junior-logo"]} ${styles["nav-menu-item"]}`} >
+                        <Image src={JuniorCaucusLogoSvg} alt="Junior Caucus Logo" />
+                    </Link>
+                </div>
+                <div className={`${styles.menu} }`}>
+                    <Link href="/" className={currPageGet(props.page, "Home")}>
+                        {getText(props.page, "Home")}
+                    </Link>
+                    <Link href="/people" className={currPageGet(props.page, "People")} >
+                        {getText(props.page, "People")}
+                    </Link>
+                    <Link href="/events" className={currPageGet(props.page, "Events")}>
+                        {getText(props.page, "Events")}
+                    </Link>
+                    <Link href="/resources" className={currPageGet(props.page, "Resources")}>
+                        {getText(props.page, "Resources")}
+                    </Link>
+                    <Link href="/Socials" className={currPageGet(props.page, "Socials")} >
+                        {getText(props.page, "Socials")}
+                    </Link>
+                </div>
+
+                <div className={`${styles["nav-menu-item"]} ${styles["mobile-dropdown"]}`}>
+                    <div>
+                        <span className={styles.decorative}>b</span> Menu{" "}
+                        <span className={styles.decorative}>a</span>
+                    </div>
+                    <div className={styles["dropdown-content"]}>
+                        <Link href="/" className={currPageGet(props.page, "Home")}>
+                            {getText(props.page, "Home")}
+                        </Link>
+                        <Link href="/people" className={currPageGet(props.page, "People")}>
+                            {getText(props.page, "People")}
+                        </Link>
+                        <Link href="/events" className={currPageGet(props.page, "Events")}>
+                            {getText(props.page, "Events")}
+                        </Link>
+                        <Link href="/resources" className={currPageGet(props.page, "Resources")}>
+                         {getText(props.page, "Resources")}
+                        </Link>
+                        <Link href="/Socials" >
+                            <div className={currPageGet(props.page, "Socials")}>{getText(props.page, "Socials")}</div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function currPageGet(page: string, currPage: string) {
+    return currPage === page ? `${styles["nav-menu-item"]} ${styles["current-page"]}` : styles["nav-menu-item"];
+};
+
+function getText(page: string, currPage: string) {
+    return currPage === page ? (
+        <>
+            <span className={styles.decorative}>b</span> {currPage}{" "}
+            <span className={styles.decorative}>a</span>
+        </>
+    ) : (
+        <>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{currPage}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </>
+    );
+};
