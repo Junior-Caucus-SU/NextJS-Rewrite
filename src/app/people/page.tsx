a37fbdec046561257da0e694577a63611dff47ba
Elias Xu <flam0799@gmail.com>
2024-03-27 20:10:49 -0400

    add peoples page

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,7 +1,9 @@
 import Nav from "@/components/Nav"
+import Gallery from "@/components/Gallery";
 
 export default async function People() {
     return (<div>
         <Nav page="People" />
+        <Gallery />
     </div>);
 }
--- src/app/socials/page.tsx
+++ src/app/socials/page.tsx
@@ -1,8 +1,12 @@
 import Nav from "@/components/Nav";
+import Script from "next/script";
+import Embeds from "@/components/Embeds";
 
 export default async function Socials() {
     return (<div>
         <Nav page="Socials"/>
+        <Embeds />
+   
     </div>)
 }
 
