9f470ec79bec1d391923a4be49173d39ce32e7a6
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-04-11 07:47:36 -0400

    add parallax but not classnames

--- src/components/HomeParallax.tsx
+++ src/components/HomeParallax.tsx
@@ -5,17 +5,90 @@ import GloriousLeaders from "/public/static/images/gloriousLeaders.png";
 import ParallaxBackground from "/public/static/images/parallaxBackgound.png";
 import Battle from "/public/static/images/battle.png";
 import PhotoCapture from "/public/static/images/photoCapture.png";
-
+import Image from "next/image";
 
 import {
     ParallaxProvider,
     ParallaxBanner,
     ParallaxBannerLayer,
     Parallax,
+    EffectNumber,
+    EffectString,
+    BannerLayer,
 } from "react-scroll-parallax";
 
 
+const images = [
+    {
+        url: GloriousLeaders,
+        scrollspeed: 5,
+        alt: "polaroid josephine, grace, ryan",
+    },
+    { url: Battle, scrollspeed: 10, alt: "battle" },
+    { url: PhotoCapture, scrollspeed: 40, alt: "photocapture" },
+];
+
+
+
+const offsets = [10, 64, 10];
+
 export default function HomeParallax() {
-    return (<div>
-    </div>);
-}
+    return (
+        <div>
+            <ParallaxProvider>
+                <ParallaxBanner
+                    className="parallaxView"
+                    layers={[
+                        {
+                            image: ParallaxBackground.src,
+                            // amount: 0.5,
+                            // slowerScrollRate: true,
+                        },
+                    ]}
+                    style={{
+                        height: "80vw",
+                    }}
+                >
+                    <ParallaxBannerLayer speed={-20}>
+                        <Image
+                            src={Homecoming}
+                            alt="homecoming"
+                            style={{
+                                width: "60vw",
+                                display: "block",
+                                marginLeft: "auto",
+                                marginRight: "auto",
+                            }}
+                        />
+                    </ParallaxBannerLayer>
+                    {images.map((image, index) => {
+                        let translateY;
+                        if (image.url === PhotoCapture) {
+                            translateY = [-75, -300];
+                        } else {
+                            translateY = [50, -50];
+                        }
+                        return (
+                            <Parallax
+                                key={index}
+                                easing={[1, -0.2, 0.5, 5]}
+                                translateY={translateY as EffectNumber | EffectString}
+                                translateX={[offsets[index], offsets[index]]}
+                            >
+                                <Image
+                                    src={image.url}
+                                    alt={image.alt}
+                                    style={{
+                                        position: "inherit",
+                                        width: "40%",
+                                    }}
+                                />
+                            </Parallax>
+                        );
+                    })}
+                </ParallaxBanner>
+            </ParallaxProvider>
+        </div>
+    );
+};
+
