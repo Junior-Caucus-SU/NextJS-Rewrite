080f688e52869c5fb2d1646cbe1bc99bf44bcc2f
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-04-20 14:39:04 -0400

    add the jsx code for the homepage (not working yet)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,86 @@
-import Image from "next/image";
+// styles
 import "@/styles/Home.css";
+
+//components
 import Nav from "@/components/Nav";
+import Schedule from "@/components/Schedule";
+import DateCrawler from "@/components/DateCrawler";
+import Specifics from "@/components/Specifics";
+import HomeParallax from "@/components/HomeParallax";
+
+// utils and data
+import Image from "next/image";
 import getData from "@/utils/getData";
 import DayScheduleInterface from "@/utils/DayScheduleInterface";
 
+// images
+import Border from "/public/static/images/BorderImg.svg";
+import TribecaBridge from "/public/static/images/StuyBridgeDrawing.png"
+
 export const revalidate = 3600;
+
+// ! TODO: FIX
+
+
+function cSpanning(text) {
+  return text.split("").map((char, index) => (
+    <span key={index} className="character">
+      {char}
+    </span>
+  ));
+}
 
 
 export default async function Home() {
   let data = await getData("Schedule") as DayScheduleInterface[];
 
   return (
-    <div>
-      <Nav page="Home" />
-    </div>
+    <>
+      <div className="homepage-div">
+        <div>
+          <Nav page="Home" />
+        </div>
+        <div className="homepage-schedule-container">
+          <div className="schedule-banner-container">
+            <div className="schedule-banner">
+              <Schedule {...data} />
+            </div>
+          </div>
+          <div className="bridge-pos">
+            <Image
+              src={TribecaBridge}
+              alt="Tribeca bridge"
+              className="tribeca-bridge"
+            />
+          </div>
+          <div className="date-crawler-pos">
+            <div className="schedule-date-crawler">
+              <DateCrawler {...data} />
+            </div>
+          </div>
+        </div>
+        <Image src={Border} alt="Border" className="border1" />
+        <Specifics />
+        <div className="mission-box">
+          <h1 className="mission-statement">
+            {cSpanning(
+              "We represent the Junior class body and are dedicated to easing the challenges of junior year by alleviating stress through valuable resources and fun events! We are also prioritizing open communication and transparency from effective team collaboration to fostering clear and consistent communication with the student body!",
+            )}
+          </h1>
+        </div>
+        <div className="parallax">
+          <HomeParallax />
+        </div>
+        <div className="end-scroll">
+          <div className="polaroids-sky polaroids polaroid-Image"></div>
+          <div className="polaroids-text polaroids polaroid-Image">
+            We'll stay with you this year
+          </div>
+          <div className="polaroids-bottom polaroids"></div>
+        </div>
+      </div>
+    </>
   );
 }
+
+
--- src/components/DateCrawler.tsx
+++ src/components/DateCrawler.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from "next/image";
 import styles from "@/styles/DateCrawler.module.css";
 import DayScheduleInterface from "@/utils/DayScheduleInterface";
 import Crawl from "/public/static/images/EllipseCrawl.svg";
@@ -6,9 +7,59 @@ import lightCrawl from "/public/static/images/lightEllipseCrawl.svg";
 import { useEffect, useState } from "react";
 
 export default function DateCrawler(props: DayScheduleInterface[]) {
+    const [dayType, setDayTypes] = useState<string[][]>();
+    const [date, setDate] = useState(new Date());
+    useEffect(() => {
+      const fetchSheetsData = async () => {
+        try {
+          const ID = "1yB7zzw0I3hUjLwgKZXMpBioQ9FNkTg2bp3skTwtatHk";
+          const sheet_name = "Schedule";
+          const response = await fetch(
+            `https://docs.google.com/spreadsheets/d/${ID}/gviz/tq?tqx=out:csv&sheet=${sheet_name}`,
+          );
+          const text = await response.text();
+          let d = text.split("\n");
+          let data = d.map((x) => {
+            return x.replaceAll('"', "").split(",");
+          });
+          data.shift();
+          setDayTypes(data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchSheetsData();
+    }, []);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setDate(new Date());
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+  
     return (
-        <div>
-
-        </div>
-    )
+      <div>
+        {dayType?.map(
+          (day, i) =>
+            i !== 0 &&
+            i !== 6 && (
+              <div className={day[0].toLowerCase() + " day-box"} key={i}>
+                <div className="ellipse-crawler">
+                  <Image
+                    src={date.getDay() < i ? lightCrawl : Crawl}
+                    alt={date.getDay() < i ? "light ellipse" : "normal ellipse"}
+                    className="ellipse-item"
+                  />
+                  <p className="dateText">{day[0]}</p>
+                </div>
+                <p className="day-text">
+                  <span className="type-text">{day[1].trim()}</span>
+                  <span className="day-type-text">{" " + day[2]}</span>
+                </p>
+              </div>
+            ),
+        )}
+      </div>
+    );
 }
