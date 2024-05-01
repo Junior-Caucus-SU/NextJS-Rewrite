8eb53b665e779349ccacbfc818058ac0eaf9d258
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-05-01 07:52:00 -0400

    start jprom page

--- src/app/jprom/page.tsx
+++ src/app/jprom/page.tsx
@@ -1,9 +1,32 @@
 import Nav from "@/components/Nav";
+import "@/styles/JProm.css"
+import AnnouncementLine from "/public/static/images/announcementsLine.svg";
+import Image from "next/image";
 
 export default function Jprom(){
     return (
         <div>
             <Nav page="JProm"/>
+            <div className="jprom-super">
+                <div className = "jprom-container">
+                    <div className="header">JProm</div>
+                    <Image src={AnnouncementLine} alt="announcement line" className="header-border" />
+                    <div className = "description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                    <div className="funding-goals-container">
+                        <div className = "progress-bar">
+                            <div className="progress-inside">
+
+                            </div>
+                        </div>
+                        <div className="progress-description">
+                            JProm funding requirements!
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/ProgressBar.module.css";
+
+
+export default async function ProgressBar() {
+    return (
+        <div>
+
+        </div>
+    )
+}
