f2188c8e6c2f3268c74304d1eb0c324905d7b7ab
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-05-01 14:37:59 -0400

    add jprom page

--- src/app/jprom/page.tsx
+++ src/app/jprom/page.tsx
@@ -3,13 +3,21 @@ import "@/styles/JProm.css"
 import AnnouncementLine from "/public/static/images/announcementsLine.svg";
 import Image from "next/image";
 import Papa from "papaparse";
+export const revalidate = 3600 // revalidate the data at most every hour
+
 
 export default async function Jprom() {
-
+    let text = 0;
     try {
-
+        const ID = "1yB7zzw0I3hUjLwgKZXMpBioQ9FNkTg2bp3skTwtatHk";
+        const sheet_name = "JPPROGRESS";
+        const response = await fetch(
+            `https://docs.google.com/spreadsheets/d/${ID}/gviz/tq?tqx=out:csv&sheet=${sheet_name}`,
+            { cache: 'no-store' }
+        );
+        text = ((Papa.parse(await response.text()).data as any[][])[1][0] as number);
     } catch (e) {
-
+        console.log(e);
     }
 
     return (
@@ -21,12 +29,12 @@ export default async function Jprom() {
                         <div className="header">JProm</div>
                         <Image src={AnnouncementLine} alt="announcement line" className="header-border" />
                         <div className="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            One of the main goal of our team this year is to raise funds for Junior Prom (JProm) that will be happening in the spring. JProm is the highly anticipated event-of-the-year event that acts as a reward for students’ hard work throughout the year. However, in recent years, JProm costs have greatly inflated. Our team strongly believes in the accessibility of this event, and we have decided to make fundraising one of our biggest focuses this year. We have been brainstorming a variety of fundraising events, from polaroid fundraisers to frequent bake sales. Though there is no guarantee that JProm will be fully affordable to all students, we are trying our best to fundraise as much as we can.
                         </div>
                     </div>
                     <div className="funding-goals-container">
                         <div className="progress-bar">
-                            <div className="progress-inside" />
+                            <div className="progress-inside" style={{height:`${100 - text}%`}}/>
                         </div>
                         <div className="progress-description">
                             JProm funding requirements!
